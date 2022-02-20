dfd11987b78ac1ab57e3f78c47dd9970338fce6a
Michal Charemza <michal@charemza.name>
2022-02-20 09:01:25 +0000

    latest

--- combine.py
+++ combine.py
@@ -5,6 +5,7 @@ import re
 
 _RE_COMBINE_WHITESPACE = re.compile(r"\s+")
 file_dir = 'data'
+target_db = 'dft-mot-results-and-test-items-2020.sqlite'
 target_db = 'dft-mot-results-and-test-items-2005-to-2020.sqlite'
 
 test_files = [
@@ -63,64 +64,109 @@ test_item_files = [
     (',', 'dft_test_item-from-2020-10-01_00-00-00-to-2021-01-01_00-00-00.csv'),
 ]
 
-try:
-    os.unlink(target_db)
-except FileNotFoundError:
-    pass
+# try:
+#     os.unlink(target_db)
+# except FileNotFoundError:
+#     pass
+
+def skip_first(it):
+    next(it)
+    yield from it
 
 with sqlite3.connect(target_db) as con:
-    print('Creating database')
-    cur = con.cursor()
-    cur.execute('BEGIN')
-    cur.execute('''
-        CREATE TABLE tests (
-            test_id INTEGER NOT NULL,
-            vehicle_id INTEGER NOT NULL,
-            test_date DATE NOT NULL,
-            test_class_id INTEGER NOT NULL,
-            test_type TEXT NOT NULL,
-            test_result TEXT NOT NULL,
-            test_mileage INTEGER NOT NULL,
-            postcode_area TEXT NOT NULL,
-            make TEXT NOT NULL,
-            model TEXT NOT NULL,
-            colour TEXT NOT NULL,
-            fuel_type TEXT NOT NULL,
-            cylinder_capacity INTEGER NOT NULL,
-            first_use_date INTEGER NOT NULL
-        )
-    ''')
-    cur.execute('''
-        CREATE TABLE test_items (
-            test_id INTEGER NOT NULL,
-            rfr_id INTEGER NOT NULL,
-            rfr_type_code TEXT NOT NULL,
-            location_id INTEGER NOT NULL,
-            dangerous_mark TEXT NOT NULL
-        )
-    ''')
-    for delimeter, file_name in test_files:
-        print(file_name)
-        with open(f'{file_dir}/{file_name}', 'r') as f:
-            reader = csv.reader(f, delimiter=delimeter, quoting=csv.QUOTE_NONE)
-            for row in reader:
-                # There are issues with make and model - sometime model seems split into
-                # too many extra columns - suspect file was generated without escaping the
-                # delimeter
-                if len(row) > 14:
-                    make_cols = len(row) - 14
-                    before_make = row[:9]
-                    make = row[9:9+make_cols+1]
-                    make = [_RE_COMBINE_WHITESPACE.sub(' ', delimeter.join(make)).strip()]
-                    after_make = row[9+make_cols+1:]
-                    row = before_make + make + after_make
-                    print('Cleaned', row)
-                cur.executemany("INSERT INTO tests VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (row,))
+#     print('Creating database')
+#     cur = con.cursor()
+#     cur.execute('BEGIN')
+#     cur.execute('''
+#         CREATE TABLE tests (
+#             test_id INTEGER NOT NULL,
+#             vehicle_id INTEGER NOT NULL,
+#             test_date DATE NOT NULL,
+#             test_class_id INTEGER NOT NULL,
+#             test_type TEXT NOT NULL,
+#             test_result TEXT NOT NULL,
+#             test_mileage INTEGER NOT NULL,
+#             postcode_area TEXT NOT NULL,
+#             make TEXT NOT NULL,
+#             model TEXT NOT NULL,
+#             colour TEXT NOT NULL,
+#             fuel_type TEXT NOT NULL,
+#             cylinder_capacity INTEGER NOT NULL,
+#             first_use_date INTEGER NOT NULL
+#         )
+#     ''')
+#     cur.execute('''
+#         CREATE TABLE test_items (
+#             test_id INTEGER NOT NULL,
+#             rfr_id INTEGER NOT NULL,
+#             rfr_type_code TEXT NOT NULL,
+#             location_id INTEGER NOT NULL,
+#             dangerous_mark TEXT NOT NULL
+#         )
+#     ''')
+#     for delimeter, file_name in test_files:
+#         print(file_name)
+#         with open(f'{file_dir}/{file_name}', 'r') as f:
+#             reader = csv.reader(f, delimiter=delimeter, quoting=csv.QUOTE_NONE)
+#             for row in reader:
+#                 # There are issues with make and model - sometime model seems split into
+#                 # too many extra columns - suspect file was generated without escaping the
+#                 # delimeter
+#                 if len(row) > 14:
+#                     make_cols = len(row) - 14
+#                     before_make = row[:9]
+#                     make = row[9:9+make_cols+1]
+#                     make = [_RE_COMBINE_WHITESPACE.sub(' ', delimeter.join(make)).strip()]
+#                     after_make = row[9+make_cols+1:]
+#                     row = before_make + make + after_make
+#                     print('Cleaned', row)
+#                 cur.executemany("INSERT INTO tests VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (row,))
 
-    for delimeter, file_name in test_item_files:
-        print(file_name)
-        with open(f'{file_dir}/{file_name}', 'r') as f:
-            reader = csv.reader(f, delimiter=delimeter)
-            cur.executemany("INSERT INTO test_items VALUES (?, ?, ?, ?, ?)", reader)
-    cur.execute('COMMIT')
-    print('Done')
+#     for delimeter, file_name in test_item_files:
+#         print(file_name)
+#         with open(f'{file_dir}/{file_name}', 'r') as f:
+#             reader = csv.reader(f, delimiter=delimeter)
+#             cur.executemany("INSERT INTO test_items VALUES (?, ?, ?, ?, ?)", reader)
+#     cur.execute('COMMIT')
+#     print('Done')
+    cur = con.cursor()
+    cur.execute('''
+        DROP TABLE IF EXISTS item_details
+    ''')
+    cur.execute('''
+        CREATE TABLE item_details (
+            rfr_id INTEGER NOT NULL,
+            test_class_id INTEGER NOT NULL,
+            test_item_id INTEGER NOT NULL,
+            minor_item TEXT NOT NULL,
+            rfr_desc TEXT NOT NULL,
+            rfr_loc_marker TEXT NOT NULL,
+            rfr_insp_manual_desc TEXT NOT NULL,
+            rfr_advisory_text TEXT NOT NULL,
+            test_item_set_section_id TEXT NOT NULL,
+            PRIMARY KEY (rfr_id, test_class_id)
+        )
+    ''')
+    with open(f'{file_dir}/dft_item_detail.csv', 'r') as f:
+        reader = csv.reader(f, delimiter='|', quoting=csv.QUOTE_NONE)
+        cur.executemany("INSERT INTO item_details VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", skip_first(reader))
+
+    cur = con.cursor()
+    cur.execute('''
+        DROP TABLE IF EXISTS group_details
+    ''')
+    cur.execute('''
+        CREATE TABLE group_details (
+            test_item_id INTEGER NOT NULL,
+            test_class_id INTEGER NOT NULL,
+            parent_id INTEGER NOT NULL,
+            test_item_set_section_id TEXT NOT NULL,
+            item_name TEXT NOT NULL,
+            PRIMARY KEY (test_item_id, test_class_id)
+        )
+    ''')
+    with open(f'{file_dir}/dft_group_detail.csv', 'r') as f:
+        reader = csv.reader(f, delimiter='|', quoting=csv.QUOTE_NONE)
+        cur.executemany("INSERT INTO group_details VALUES (?, ?, ?, ?, ?)", skip_first(reader))
+
+
